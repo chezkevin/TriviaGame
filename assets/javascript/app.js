c5f8cf418b572335614af0663ae20c506f2304dc
Kevin Tam <tam580@gmail.com>
2016-11-03 16:31:15 -0500

    answer images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,9 +24,9 @@ var quiz = {
 	answer: ["Peter Parker",
 				"Foggy Nelson",
 				"Spider-size"],
-	answerImage: ['<img src="assets/images/Spider-Man_Unmasked.png">',
-					'<img src="assets/images/Nelson_and_Murdock.png">',
-					'<img src="assets/images/spider-size.png">'],
+	answerImage: ['<img src="./assets/images/Spider-Man_Unmasked.png">',
+					'<img src="./assets/images/Nelson_and_Murdock.png">',
+					'<img src="./assets/images/spider-size.png">'],
 
 	start: function(){
 		$('#start').css("display","none");
